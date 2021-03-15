d22ca8382a524c6725cd1a01e9fbf363757fb336
Vittorio Antonio <52713232+SteadyAtol143@users.noreply.github.com>
2021-03-15 17:52:36 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,21 +2,46 @@ package main
 
 import(
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main () {
-	var price int
-	var given int
+	var command string
+	var randomCoinVal int
+	eagVal := 0
+	nonEagVal := 0
+	srnd1 := rand.NewSource(time.Now().UnixNano())
+	rnd1 := rand.New(srnd1)
 
-	fmt.Println("Стоимость товара: ")
-	fmt.Scan(&price)
-	fmt.Println("Пользователь дал: ")
-	fmt.Scan(&given)
-
-	fmt.Println("Сдача: ", given - price)
-	
-	fmt.Println("Можно выдать: ")
-	fmt.Println("Десятирублёвых монет: ", (given-price)/(10))
-	fmt.Println("Пятирублёвых монет: ", ((given-price) - 10*((given-price)/(10)))/5)
-	fmt.Println("Рублёвых монет: ",  ((given-price) - 10*((given-price)/(10))) - 5*(((given-price) - 10*((given-price)/(10)))/5))
+	fmt.Println("Программа: Давайте побросаем монетку?")
+	fmt.Println("Вы: ")
+	fmt.Scan(&command)
+	if command == "No" {
+		fmt.Println("Программа: Ну ладно ;(")
+	} else if command == "Yes" {
+		for  {
+			fmt.Println("Программа: ведите th чтобы бросить монетку.")
+			fmt.Println("Программа: ведите stop чтобы прекратить работу эмулятора.")
+			fmt.Println("Вы: ")
+			fmt.Scan(&command)
+			if command == "stop" {
+				fmt.Println("Всего орлов: ", eagVal)
+				fmt.Println("Всего решек: ", nonEagVal)
+				break
+			} else {
+				randomCoinVal = rnd1.Intn(2)
+			    if randomCoinVal == 1 {
+			    	fmt.Println("Выпал орёл!")
+			    	eagVal += 1
+			    } else {
+			    	fmt.Println("Выпала решка!")
+			    	nonEagVal += 1
+			    }
+			}
+		}
+	} else {
+		fmt.Println("Программа: Я вас не поняла, но да ладно...")
+	}
 }
+
